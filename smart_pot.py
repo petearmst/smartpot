bcf3a2848cc050b8a309836e3e0e13cc14d2fe21
petearmst@gmail.com <petearmst@gmail.com>
2018-02-25 14:56:07 +0000

    First commit

---
+++ smart_pot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 14:13:15 2018
+
+@author: pete
+"""
+### Import relevant libraries
+#import automationhat # this library allows us to address the peripheral hardware daughter board on the RPi
+#import time #this library allos us to create simple delays in second values
+import sqlite3 as sq #sqlite3 allows us to store data into tables that are extensible and more manageable than simple csv files
+
+
+# set up RPi database
+
+
+
+
+# implement Rpi control logic
+
+
+
+
+
+
